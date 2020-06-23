40c1d4b1dccece530e7be4274f1167da2e6e2dff
Rafael Costa <rafael.costa2109@gmail.com>
2020-06-23 19:37:28 +0100

    .

--- src/pages/app/NewRequest.js
+++ src/pages/app/NewRequest.js
@@ -117,7 +117,7 @@ export default function NovoRequisito(){
         console.log(event.target.value)
         var tmp = "";
         tmp = event.target.value;
-        if(tmp.length>3)
+        if(tmp.length>2)
           getAllStudents(tmp);
       };
   
